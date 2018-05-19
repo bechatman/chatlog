3f60533624bc7374d5087dc0717c1b22bb5c757b
Brian <bechatman@gmail.com>
2018-05-18 23:51:31 -0500

    Update music page

---
+++ src/components/TopTracks/topTracks.js
@@ -0,0 +1,38 @@
+const lfm = (command, options) => {
+  if (options) {
+    const paramsAll = Object.keys(options).map(
+      item => `${item}=${options[item]}`
+    );
+    const params = `&${paramsAll.join('&')}`;
+    return `http://ws.audioscrobbler.com/2.0/?method=${command}&user=bechatman&api_key=25b4be602a0fd2eefc999e4f620f3275${params}&format=json`;
+  }
+  return `http://ws.audioscrobbler.com/2.0/?method=${command}&user=bechatman&api_key=25b4be602a0fd2eefc999e4f620f3275&format=json`;
+};
+
+function loadAlbums(albums) {
+  const albumListing = document.querySelector('.albums');
+  albums.forEach(item => {
+    const { name, artist, image } = item;
+    const template = `
+      <li><img src="${image[image.length - 1]['#text']}" alt="">
+      <a href="${artist.url}" class="overlay">
+        <span class="artist">${artist.name}</span>
+        <span class="album">${name}</span>
+      </a>
+    </li>`;
+    albumListing.innerHTML += template;
+    setTimeout(() => {
+      document.querySelector('.albums').classList.add('active');
+    }, 500);
+  });
+}
+
+const topTracksSection = document.querySelector('.top-tracks');
+
+if (topTracksSection) {
+  fetch(lfm('user.getTopAlbums', { period: '1month', limit: 6 }))
+    .then(response => response.json())
+    .then(results => {
+      loadAlbums(results.topalbums.album);
+    });
+}
