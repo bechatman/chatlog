224625dd967bce25c30986df93ac3af9f9348d7a
Brian <bechatman@gmail.com>
2019-03-30 19:09:55 -0400

    Update album art plugin

---
+++ src/components/AlbumPostArt/albumPostArt.js
@@ -0,0 +1,28 @@
+import { lfm, addAlbumArt } from '../../assets/utilities';
+
+// If there is an artist and album, find the album art.
+
+const albums = document.querySelectorAll('.album');
+
+if (albums) {
+  Array.from(albums).map(item => {
+    if (item.dataset.album !== 'false' && item.dataset.artist !== 'false') {
+      fetch(lfm('album.search', { album: item.dataset.album }))
+        .then(response => response.json())
+        .then(response => response.results.albummatches.album[0])
+        .then(album => {
+          if (album) {
+            addAlbumArt(
+              album,
+              item,
+              `${item.dataset.caption ? `${item.dataset.caption}<br />` : ''}${
+                album.artist
+              }, <a href="${album.url}">"${album.name}"</a>/Last.fm`
+            );
+          }
+        })
+        .catch(error => console.error(error));
+    }
+    return item;
+  });
+}
--- src/components/TopTracks/topTracks.js
+++ src/components/TopTracks/topTracks.js
@@ -1,13 +1,4 @@
-const lfm = (command, options) => {
-  if (options) {
-    const paramsAll = Object.keys(options).map(
-      item => `${item}=${options[item]}`
-    );
-    const params = `&${paramsAll.join('&')}`;
-    return `https://ws.audioscrobbler.com/2.0/?method=${command}&user=bechatman&api_key=25b4be602a0fd2eefc999e4f620f3275${params}&format=json`;
-  }
-  return `https://ws.audioscrobbler.com/2.0/?method=${command}&user=bechatman&api_key=25b4be602a0fd2eefc999e4f620f3275&format=json`;
-};
+import { lfm } from '../../assets/utilities';
 
 function loadAlbums(albums) {
   const albumListing = document.querySelector('.albums');
@@ -27,19 +18,6 @@ function loadAlbums(albums) {
   });
 }
 
-function addAlbumArt(album, element) {
-  if (album && element) {
-    const { image } = album;
-    const figure = document.createElement('figure');
-    figure.innerHTML += `<img src="${
-      image[image.length - 1]['#text']
-    }" alt="Album Cover" aria-describedby="copyright" />
-    <figcaption id="copyright">Art provided by Last.fm</figcaption>`;
-    figure.classList.add('albumArt');
-    element.prepend(figure);
-  }
-}
-
 const topTracksSection = document.querySelector('.top-tracks');
 
 if (topTracksSection) {
@@ -48,26 +26,4 @@ if (topTracksSection) {
     .then(results => {
       loadAlbums(results.topalbums.album);
     });
-}
-
-// If there is an artist and album, find the album art.
-
-const blogPost = document.querySelector('.blogPost .container');
-
-if (
-  blogPost &&
-  blogPost.dataset.album !== 'false' &&
-  blogPost.dataset.artist !== 'false'
-) {
-  fetch(lfm('album.search', { album: blogPost.dataset.album }))
-    .then(response => response.json())
-    .then(response => response.results.albummatches.album)
-    .then(results =>
-      results.filter(
-        album =>
-          album.artist === blogPost.dataset.artist &&
-          album.name === blogPost.dataset.album
-      )
-    )
-    .then(album => addAlbumArt(album[0], blogPost));
 }
